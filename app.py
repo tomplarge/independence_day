0cf9b9d52d80c20afae4b1b86c542ca7fa4e9f77
Tom Large <tomlarge@Toms-MacBook-Pro.local>
2017-10-03 16:23:42 -0500

    flask app, restructuring lambda code

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 from __future__ import print_function
+from flask import Flask, request
+from flask_restful import Resource, Api
+import os, sys, logging
 
+# initialize flask app
+flask_app = Flask(__name__)
+flask_api = Api(flask_app)
+
+class Navigator(Resource):
+    def get(self):
+        return "success"
+
+# define endpoint
+navigator_endpoint = "/navigator"
+
+# add endpoint to flask
+flask_api.add_resource(Navigator, navigator_endpoint)
+
+if __name__ == '__main__':
+    # run on open host on port 5000
+    flask_app.run(host = "0.0.0.0", port = 5000)
 
 # --------------- Helpers that build all of the responses ----------------------
 
@@ -72,10 +92,10 @@ def get_tool_locations():
         "duct tape": "back right quadrant.",
         "tape": "back right quadrant."
     }
-    
+
     return tools
-    
-    
+
+
 def find_tool_in_session(intent, session):
     """ Finds the tool in the session and prepares the speech to reply to the
     user.
@@ -94,7 +114,7 @@ def find_tool_in_session(intent, session):
     else:
         speech_output = "I'm not sure I know where that tool is. Try again."
         reprompt_text = "Ask me where a tool is and I will try to locate it for you."
-        
+
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
---
+++ lambda_stuff/lambda_function.py
@@ -0,0 +1,3 @@
+import requests
+r = requests.post('http://10.0.0.107:8000')
+print r
