56a1f9ba61ca2f683d4e117dc4d2a7c0acf89cba
Tom Large <tomlarge@Toms-MacBook-Pro.local>
2017-10-10 21:54:04 -0500

    adding safety, info, word distance for determining spoken tool

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
 import requests
+import numpy as np
 from flask import Flask, request
 from flask_restful import Resource, Api
 import os, sys, logging
-from pixels import pixels
-import subprocess
+#from pixels import pixels
 # import RPi.GPIO as GPIO
 import time
 # from sample import *
@@ -70,41 +70,103 @@ def handle_session_end_request():
         card_title, speech_output, None, should_end_session))
 
 
-def get_tool_location(tool_name):
-    tools = {
-        "laser cutter": "front left quadrant.",
-        "Lisa cutter": "front left quadrant.",
-        "laser color": "front left quadrant.",
-        "lisa cutter": "front left quadrant.",
-        "3d printer": "back left quadrant.",
-        "3dprinter": "back left quadrant.",
-        "3 d printer": "back left quadrant.",
-        "3 printer": "back left quadrant.",
-        "3D printer": "back left quadrant."
-    }
+def levenshtein_distance(string1, string2, deletion_cost, insertion_cost, substitution_cost):
+	"""
+	Computes levenshtein distance between string1 and string2 given costs.
+	Returns the last entry in the distance matrix, d, representing the distance
+	"""
+	d = np.zeros((len(string1) + 1, len(string2) + 1))
 
-    endpoints = {
-        "3D printer": "https://3dprinter.ngrok.io/LED",
-        "3d printer": "https://3dprinter.ngrok.io/LED",
-        "3dprinter": "https//3dprinter.ngrok.io/LED",
-        "3 d printer": "https://3dprinter.ngrok.io/LED",
-        "3 printer": "https://3dprinter.ngrok.io/LED",
-        "laser cutter": "https://lasercutter.ngrok.io/LED",
-        "Lisa cutter": "https://lasercutter.ngrok.io/LED",
-        "lisa cutter": "https://lasercutter.ngrok.io/LED",
-        "laser color": "https://lasercutter.ngrok.io/LED"
-    }
-    try:
-        endpoint = endpoints[tool_name]
-    except:
-        endpoint = None
-    try:
-        tool_location = tools[tool_name]
-    except:
-        tool_location = None
+	# initialize matrix
+	for i in range(d.shape[0]):
+		d[i,0] = i * deletion_cost
+	for j in range(d.shape[1]):
+		d[0,j] = j * insertion_cost
 
-    return tool_location, endpoint
+	for j in range(1, d.shape[1]): # counter for string2
+		for i in range(1, d.shape[0]): # counter for string1
+			if string2[j-1] == string1[i-1]:
+				# no operation cost, letters match
+				d[i,j] = d[i-1,j-1]
+			else:
+				d[i,j] = min(d[i-1,j] + deletion_cost,
+							d[i,j-1] + insertion_cost,
+							d[i-1,j-1] + substitution_cost)
 
+	return d[i,j]
+
+
+def determine_tool(word):
+    # compute levenshtein distance for word.
+    # choose lowest distance for set of tools
+    tools = get_tools()
+    substitution_cost = 1
+    deletion_cost = 1
+    insertion_cost = 1
+    errors = [levenshtein_distance(word, tools[i]["name"], deletion_cost, insertion_cost, substitution_cost) for i in range(len(tools))]
+    return tools[np.argmin(errors)]
+
+
+def get_tools():
+    tools = [{
+                "name": "3d printer",
+                "endpoint": "https://3dprinter.ngrok.io/LED",
+                "location": "right side",
+                "safety": "Don't touch while hot",
+                "info": "The 3D printer works by using a plastic filament to construct the design. " +
+                    "The plastic filament is melted, and the arm of the printer moves back and forth, " +
+                    "slowly building up the object. Once it is finished, it will be cool and dry. " +
+                    "3D printing can take anywhere from 1 hour to several days, depending on the size of the object. " +
+                    "Let me know if you need any more assistance or guidance. Happy making!"
+            },
+            {
+                "name": "laser cutter",
+                "endpoint": "https://lasercutter.ngrok.io/LED",
+                "location": "front",
+                "safety": "Don't look directly at the laser while it is cutting. This can damage your eyes.",
+                "info": "Place a piece of wood on the cutting surface, flush with the top left edge. Close the lid, and choose your design. " +
+                    "Be sure to check your settings for power and speed, as this will affect the outcome of your design. " +
+                    "Let me know if you need any more assistance or guidance. Happy making!"
+            }]
+
+    return tools
+
+
+# def get_tool_location(tool_name):
+#     tools = {
+#         "laser cutter": "front left quadrant.",
+#         "Lisa cutter": "front left quadrant.",
+#         "laser color": "front left quadrant.",
+#         "lisa cutter": "front left quadrant.",
+#         "3d printer": "back left quadrant.",
+#         "3dprinter": "back left quadrant.",
+#         "3 d printer": "back left quadrant.",
+#         "3 printer": "back left quadrant.",
+#         "3D printer": "back left quadrant."
+#     }
+#
+#     endpoints = {
+#         "3D printer": "https://3dprinter.ngrok.io/LED",
+#         "3d printer": "https://3dprinter.ngrok.io/LED",
+#         "3dprinter": "https://3dprinter.ngrok.io/LED",
+#         "3 d printer": "https://3dprinter.ngrok.io/LED",
+#         "3 printer": "https://3dprinter.ngrok.io/LED",
+#         "laser cutter": "https://lasercutter.ngrok.io/LED",
+#         "Lisa cutter": "https://lasercutter.ngrok.io/LED",
+#         "lisa cutter": "https://lasercutter.ngrok.io/LED",
+#         "laser color": "https://lasercutter.ngrok.io/LED"
+#     }
+#     try:
+#         endpoint = endpoints[tool_name]
+#     except:
+#         endpoint = None
+#     try:
+#         tool_location = tools[tool_name]
+#     except:
+#         tool_location = None
+#
+#     return tool_location, endpoint
+#
 
 def find_tool_in_session(intent, session):
     """ Finds the tool in the session and prepares the speech to reply to the
@@ -114,33 +176,26 @@ def find_tool_in_session(intent, session):
     session_attributes = {}
     reprompt_text = None
 
-    if 'Tool' in intent['slots']:
-        tool_to_find = intent['slots']['Tool']['value']
-        print(tool_to_find + "\n")
-        tool_location, tool_endpoint = get_tool_location(tool_to_find)
-        if tool_location is not None and tool_endpoint is not None:
-            speech_output = "The " + tool_to_find + " is located in the " + tool_location + ". Happy making."
-        else:
-            speech_output = "I'm sorry. I couldn't find the requested tool. Goodbye."
+    if 'Tool' in intent['slots'] and 'value' in intent['slots']['Tool']:
+        spoken_tool = intent['slots']['Tool']['value']
+        tool = determine_tool(spoken_tool)
+        speech_output = "The " + tool["name"] + " is located in the " + tool["location"] + ". Happy making."
         should_end_session = True
     else:
         speech_output = "I didn't catch that. Try again."
         should_end_session = False
-    
-    if tool_endpoint is not None:
-        pid = os.fork()
-        if pid == 0:
-            # child sends request to correct pi to light up
-            #subprocess.call(['python','pixels.py'])
-            r = requests.get(tool_endpoint)
-            os._exit(1)
-        else:
-            # parent returns to response to flask
-            return build_response(session_attributes, build_speechlet_response(
-                card_title, speech_output, reprompt_text, should_end_session))
+
+    # fork, respond to alexa in parent, and send LED request as child
+    pid = os.fork()
+    if pid == 0:
+        # child sends request to correct pi to light up
+        r = requests.get(tool["endpoint"])
+        os._exit(1)
     else:
-            return build_response(session_attributes, build_speechlet_response(
-                card_title, speech_output, reprompt_text, should_end_session))
+        # parent returns to response to flask
+        return build_response(session_attributes, build_speechlet_response(
+            card_title, speech_output, reprompt_text, should_end_session))
+
 
 def get_tool_info_in_session(intent, session):
     """ Gets tool info in session and prepares speech to reply to user """
@@ -148,9 +203,10 @@ def get_tool_info_in_session(intent, session):
     session_attributes = {}
     reprompt_text = None
 
-    if 'Tool' in intent['slots']:
-        tool_for_info = intent['slots']['Tool']['value']
-        speech_output = "The " + tool_for_info + " can be used with your brain, you fuh king idiot. Happy making."
+    if 'Tool' in intent['slots'] and 'value' in intent['slots']['Tool']:
+        spoken_tool = intent['slots']['Tool']['value']
+        tool = determine_tool(spoken)
+        speech_output = tool["info"]
         should_end_session = True
     else:
         speech_output = "I didn't catch that. Try again."
